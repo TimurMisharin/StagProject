5e16588633234535a01bd701ece888609f385a0e
DimaPindrik <dyebolical@gmail.com>
2019-03-17 11:21:45 +0200

    basic view templates

--- app/views.py
+++ app/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# views
+posts = [
+	{
+		'author': 'Dima',
+		'title': 'Blog 1',
+		'content': 'first post content'
+	},
+	{
+		'author': 'Timur',
+		'title': 'Blog 2',
+		'content': 'second post content'
+	}
+]
+
 def home(request):
-    return render(request, 'home/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'home/home.html', context)
 
 def about(request):
 	return render(request, 'about/about.html')
+
